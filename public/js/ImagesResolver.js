616a35e03f79b53dbe1cae9d89b7ae0da6818f22
bilousbv <bilousbv@gmail.com>
2025-05-20 23:23:50 +0300

    Fixed auto-tests

--- public/js/ImagesResolver.js
+++ public/js/ImagesResolver.js
@@ -1,25 +1,48 @@
 window.ImagesResolver = (function () {
   class ImagesResolver {
-    constructor() {
-
+    constructor(config) {
+      this.config = config;
+      this.sources = {
+        local: this.localSearch.bind(this),
+        pixabay: this.pixabaySearch.bind(this),
+      }
     }
 
-    search(query) {
-      return {
-        query: 'example',
-        images: [
-          {
-            id: 1,
-            url: '/img/mammal-3162194_640.jpg',
-            tags: 'panda'
-          },
-          {
-            id: 2,
-            url: '/img/panda-659186_640.png',
-            tags: 'panda'
-          }
-        ]
-      };
+    search(query, searchModuleId, signal) {
+      if (!query) {
+        return Promise.resolve({
+          query,
+          images: []
+        });
+      }
+
+      const searchFunk = this.sources[searchModuleId];
+
+      if (!searchFunk) {
+        throw new Error(`Unknown search module id: ${searchModuleId}`)
+      }
+
+      return Promise.resolve(searchFunk(query, signal)).then((images) => ({
+        query,
+        images
+      }));
+    }
+
+    localSearch(query) {
+      return this.digestImages(window.localDB
+        .filter((item) => item.tags.split(',').some((tag) => tag.trim().toLowerCase() === query.trim().toLowerCase())))
+    }
+
+    pixabaySearch (query, signal) {
+      const { apiKey, limit } = this.config.pixabay
+      const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=all&per_page=${limit}`
+      return fetch(url, { signal })
+          .then((res) => res.json())
+          .then(({ hits }) => this.digestImages(hits))
+    }
+
+    digestImages (images) {
+      return images.map(({ id, previewURL: url, tags }) => ({ id, url, tags }));
     }
   }
 
